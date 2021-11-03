8710bb763786cc7550b98877fc541f40131523d3
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2021-11-03 19:15:23 -0300

    formularios

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image, StyleSheet, ActivityIndicator, FlatList, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, Image, StyleSheet, TextInput } from 'react-native';
 
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            email: '',
+            password: '',
         }
     }
 
     render() {
         return (
             <View style={styles.background}>
-                <Text>Login</Text>
+                <View style={styles.formContainer}>
+                    <TextInput style={styles.input}
+                        placeholder='Email'
+                        keyboardType='email-address'
+                        onChangeText={text => this.setState({ email: text })}
+                        type='email'
+                    />
+                    <TextInput style={styles.input}
+                        placeholder='Password'
+                        secureTextEntry={true}
+                        onChangeText={text => this.setState({ password: text })}
+                    />
+
+                    <TouchableOpacity style={styles.boton} >
+                        <Text>Log in</Text>
+                    </TouchableOpacity>
+                </View>
 
             </View>
         )
@@ -20,7 +37,34 @@ class Login extends Component {
 }
 
 const styles = StyleSheet.create({
-
+    formContainer: {
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        paddingVertical: 20
+    },
+    input: {
+        height: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 6,
+        width: '100%',
+        margin: 5,
+    },
+    boton: {
+        backgroundColor: '#28a745',
+        color: '#fff',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        width: '30%',
+        textAlign: 'center',
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#28a745',
+        marginTop: 10,
+    },
 })
 
 export default Login;
