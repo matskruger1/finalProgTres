59a696386e76749ff16a2fdbed0a03700e08ed4c
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2021-11-22 16:23:13 -0300

    yarn

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Text, View, TouchableOpacity, Image, StyleSheet, ActivityIndicator, FlatList, TextInput } from 'react-native';
 
-const Drawer = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 import Home from '../screens/home'
 import Register from '../screens/register'
